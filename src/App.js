7082ced0d7020aaf1213a553cac0ea5aa4695430
Shaun Wassell <shaun@trove.ai>
2021-08-04 15:41:36 -0400

    Beginning code for video

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { CounterButton } from './CounterButton';
+import './App.css';
+
+const App = () => {
+	return (
+		<>
+		<h1>State Management Example</h1>
+		<CounterButton />
+		</>
+	);
+}
+
+export default App;
